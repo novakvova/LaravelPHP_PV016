0fb140f03bc3e98f852804d8baa83915b75610b7
novakvova <novakvova@gmail>
2022-12-23 19:28:31 +0200

    Begin show data redux

--- my-app/src/components/home/HomePage.tsx
+++ my-app/src/components/home/HomePage.tsx
@@ -1,7 +1,47 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import http from "../../http_common";
+import { IProductItem } from "./store/types";
+
 const HomePage = () => {
+
+    const {list} = useTypedSelector(store=>store.product);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        //console.log("UseEffect Render app", "DidMount");
+        http.get<Array<IProductItem>>("/api/products")
+            .then((resp) => {
+                //console.log("Response data", resp);
+                dispatch({type: "PRODUCT_LIST", payload: resp.data});
+            });
+    },[]);
+    const data = list.map(product=> (
+        <tr key={product.id}>
+            <td>{product.id}</td>
+            <td>{product.name}</td>
+            <td>{product.detail}</td>
+        </tr>
+    ));
+
+    
     return (
+        
         <>
-            <h1>Головна сторінка</h1>
+           {console.log("Render app", "One render")} 
+            <h1 className="text-center">Головна сторінка</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data}
+                </tbody>
+            </table>
         </>
     );
 }
