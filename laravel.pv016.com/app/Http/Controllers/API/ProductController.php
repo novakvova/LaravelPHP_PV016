818bee4cc974ba91a13b25a369f7951be93e3131
novakvova <novakvova@gmail>
2023-01-09 20:55:27 +0200

    Working add swagger

--- laravel.pv016.com/app/Http/Controllers/API/ProductController.php
+++ laravel.pv016.com/app/Http/Controllers/API/ProductController.php
@@ -9,6 +9,24 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     /**
+     * @OA\Post(
+     *     path="/user",
+     *     tags={"user"},
+     *     summary="Create user",
+     *     description="This can only be done by the logged in user.",
+     *     operationId="createUser",
+     *     @OA\Response(
+     *         response="default",
+     *         description="successful operation"
+     *     ),
+     *     @OA\RequestBody(
+     *         description="Create user object",
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/User")
+     *     )
+     * )
+     */
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
